5c806c9232ba41b86549339e6e9f49db72d9d8b6
Rivka Darshan <rivka3188@gmail.com>
2025-11-14 10:40:22 +0200

    update enviroment.prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shalombabait-backend-production.up.railway.app'
+  apiUrl: 'https://shalombabait-backend-production.up.railway.app/api'
 };
