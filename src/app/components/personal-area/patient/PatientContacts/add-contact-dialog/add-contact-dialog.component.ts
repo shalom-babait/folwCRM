60960445abc59f6783897f155358d011df5e1186
Rivka Darshan <rivka3188@gmail.com>
2025-12-24 12:44:48 +0200

    add confirm dialog

---
+++ src/app/components/personal-area/patient/PatientContacts/add-contact-dialog/add-contact-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contact-dialog',
+  templateUrl: './add-contact-dialog.component.html',
+  styleUrls: ['./add-contact-dialog.component.css']
+})
+export class AddContactDialogComponent {
+
+}
