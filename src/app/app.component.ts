1adb7b2c5433fd903c48f8df56a937dfb18da43c
Rivka Darshan <rivka3188@gmail.com>
2026-02-11 09:32:36 +0200

    task name patint in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { PatientInitService } from './core/patient-init.service';
 import { Router } from '@angular/router';
 import { Activities } from './classes/activities';
 
@@ -7,8 +8,14 @@ import { Activities } from './classes/activities';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'folwCRM';
+
+  constructor(private patientInit: PatientInitService) {}
+
+  ngOnInit(): void {
+    this.patientInit.loadAllPatientsToState();
+  }
 }
 
 
